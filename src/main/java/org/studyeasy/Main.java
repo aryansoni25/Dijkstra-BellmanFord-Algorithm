49dbb7fcccb9dc93eccbc9549b0b806f039a6650
aryansoni25 <ashoksoni9425@gmail.com>
2026-07-19 18:46:10 +0530

    Floydd Warshall Added

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -9,23 +9,24 @@ public class Main {
         nodeList.add(new WeightedNode("B",1));
         nodeList.add(new WeightedNode("C",2));
         nodeList.add(new WeightedNode("D",3));
-        nodeList.add(new WeightedNode("E",4));
-        nodeList.add(new WeightedNode("F",5));
-        nodeList.add(new WeightedNode("G",6));
+//        nodeList.add(new WeightedNode("E",4));
+//        nodeList.add(new WeightedNode("F",5));
+//        nodeList.add(new WeightedNode("G",6));
 
         WeightedGraph g=new WeightedGraph(nodeList);
-        g.addWeightedEdge(0,1,2);
-        g.addWeightedEdge(0,2,5);
-        g.addWeightedEdge(1,2,6);
-        g.addWeightedEdge(1,3,1);
-        g.addWeightedEdge(1,4,3);
-        g.addWeightedEdge(2,5,8);
-        g.addWeightedEdge(3,4,4);
-        g.addWeightedEdge(4,6,9);
-        g.addWeightedEdge(5,6,7);
-
-        System.out.println("Printing Dijkstra from Source : A");
-        g.dijkstra(nodeList.get(0));
+        g.addWeightedEdge(0,3,1);
+        g.addWeightedEdge(0,1,8);
+        g.addWeightedEdge(1,2,1);
+        g.addWeightedEdge(2,0,4);
+        g.addWeightedEdge(3,1,2);
+        g.addWeightedEdge(3,2,9);
+//        g.addWeightedEdge(3,4,4);
+//        g.addWeightedEdge(4,6,9);
+//        g.addWeightedEdge(5,6,7);
+        System.out.println("Printing Floydd Warshall");
+        g.floyddWarshall();
+//        System.out.println("Printing Dijkstra from Source : A");
+//        g.dijkstra(nodeList.get(0));
 //        System.out.println("Printing Bellman Ford from Source : A");
 //        g.BellmanFord(nodeList.get(0));
 
--- src/main/java/org/studyeasy/WeightedGraph.java
+++ src/main/java/org/studyeasy/WeightedGraph.java
@@ -81,4 +81,40 @@ public class WeightedGraph {
             System.out.println();
         }
     }
+
+    void floyddWarshall(){
+        int size= nodeList.size();
+        int[][] v=new int[size][size];
+        for(int i=0;i<size;i++){
+            WeightedNode first=nodeList.get(i);
+            for(int j=0;j<size;j++){
+                WeightedNode second=nodeList.get(j);
+                if(i==j){
+                    v[i][j]=0;
+                } else if (first.weightMap.containsKey(second)) {
+                    v[i][j]=first.weightMap.get(second);
+                }else{
+                    v[i][j]=Integer.MAX_VALUE/10;
+                }
+
+            }
+        }
+
+        for(int k=0;k< nodeList.size();k++){
+            for(int i=0;i< nodeList.size();i++){
+                for(int j=0;j< nodeList.size();j++){
+                    if(v[i][j]>v[i][k]+v[k][j]){
+                        v[i][j]=v[i][k]+v[k][j];
+                    }
+                }
+            }
+        }
+        for(int i=0;i<size;i++){
+            System.out.print("Printing distance list for node "+nodeList. get(i)+ ":");
+            for(int j=0;j<size;j++){
+                System.out.print(v[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
